3926582fd534850a3d0e20eec9f5d1c1ceb24dfb
hassan_aftah <aftahhassan25@gmail.com>
2026-06-29 12:25:14 +0100

    Lab1

---
+++ app/Http/Controllers/Api/BlueprintController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blueprint;
+use Illuminate\Http\Request;
+
+class BlueprintController extends Controller
+{
+    public function index()
+    {
+        $blueprints = auth()->user()->blueprints()->latest()->get();
+
+        return response()->json($blueprints);
+    }
+
+    public function show(Blueprint $blueprint)
+    {
+        return response()->json($blueprint);
+    }
+}
